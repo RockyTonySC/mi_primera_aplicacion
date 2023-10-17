2fa7022f9a54851a805732b357be282d1892b80a
RockyTonySC <rockytonysc@gmail.com>
2023-10-16 23:21:42 -0500

    nuevo cambio

--- src/main/java/demo/pe/com/miempresa/App.java
+++ src/main/java/demo/pe/com/miempresa/App.java
@@ -5,6 +5,9 @@ class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello Word");
+        
+        System.out.println("Cambio nuevo");
+        
 	}
 
 }
